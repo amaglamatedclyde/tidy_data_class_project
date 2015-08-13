c5bd4dd450b20b1513a4bb7a65baca3f1c755fdf
amalgamatedclyde <clyde@amalgamated-clyde.com>
2015-08-13 15:28:43 -0400

    initial commits

---
+++ development_script.R
@@ -0,0 +1,61 @@
+# setwd(<choose an empty directory for the project>)
+# download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "project_data.zip", method = "curl")
+# data_folders <- list.files() #returns only one filename, "project_data.zip" because we started with an empty directory
+# 
+# # we unzip the downloaded data file which creates a directory structure in our project folder. 
+# # inspection of the README files shows that the data tables we are interested in merging and their label data
+# # are found in the 4 paths below
+# 
+# unzip(data_folders) 
+
+library(dplyr)
+
+#first load the tables from the test set folder. they are X_test, y_test, subject_test and 
+test_set_path <- "UCI HAR Dataset/test/X_test.txt"
+test_set_label_path <- "UCI HAR Dataset/test/y_test.txt"
+test_set_subject_path <- "UCI HAR Dataset/test/subject_test.txt"
+test_set <- read.table(test_set_path) 
+test_set_labels <- read.table(test_set_label_path)
+test_set_subjects <- read.table(test_set_subject_path)
+label <- as.vector(test_set_labels[ ,1])
+subject <- as.vector(test_set_subjects[ ,1])
+test_set_complete <-  mutate(test_set, subject, label) 
+
+# now we perform the same operations on the training set
+train_set_path <- "UCI HAR Dataset/train/X_train.txt"
+train_set_label_path <- "UCI HAR Dataset/train/y_train.txt"
+train_set_subject_path <- "UCI HAR Dataset/train/subject_train.txt"
+train_set <- read.table(train_set_path) 
+train_set_labels <- read.table(train_set_label_path)
+train_set_subjects <- read.table(train_set_subject_path)
+label <- as.vector(train_set_labels[ ,1])
+subject <- as.vector(train_set_subjects[ ,1])
+train_set_complete <-  mutate(train_set, subject, label) 
+
+# now merge the train and test sets
+complete_set <- merge(test_set_complete, train_set_complete, all=TRUE) #merge the train and test sets
+# identical(colnames(train_set), colnames(test_set)) let's just be sure the data colnames are identical!
+
+# change the colnames to the descriptive names of the corresponding features
+# so we can select the variables of interest, mean and std, by name
+feature_label_path <- "UCI HAR Dataset/features.txt"
+feature_table <- read.table(feature_label_path, stringsAsFactors = FALSE)
+feature_names <- as.vector(feature_table[,2])
+feature_names <- append(feature_names, c("subject", "label"), after = length(feature_names))#add the names of the 2 columns we created
+if(!identical(make.names(feature_names), feature_names))
+  {feature_names <-  make.names(feature_names, unique = TRUE)} #be sure the var names are valid R names, some original names contain ()
+complete_set_with_feature_named_cols <- `colnames<-`(complete_set, feature_names) #finally add the feature names as colnames
+
+# rm(complete_set) #optionaly clean-up environment
+
+variables_of_interest <- select(complete_set_with_feature_named_cols, contains("std.."), contains("mean.."), subject, label) #select columns with mean and std
+# rm(complete_set_with_feature_named_cols) #optionaly clean-up environment
+
+# use descriptive activity names instead of "label" integer value
+activity_table <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+activity_list <- as.list(activity_table[,2]) # here was take a 2-col table and make a list so we can look up names based on integers
+label_column <- variables_of_interest[["label"]] #extract the label col as a vector
+exchange <- function(item){activity_list[[item]]} #create a utility function to transform labels into activity names
+activity <- sapply(label_column, exchange) #create activity column as vector
+variables_of_interest <- variables_of_interest %>% mutate(activity) %>% select(-label) %>%
+  arrange(subject, activity)#add activity col, delete label col, arrange rows by subject and activity
